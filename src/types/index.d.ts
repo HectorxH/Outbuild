9ae3186843841bc8d3021afcc1d15754bff60978
hectorxh <hectorplj@gmail.com>
2024-11-03 01:30:34 -0300

    lint: remove unused import

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,6 @@
 import "express-serve-static-core";
 import { JWTPayload } from "../common/middleware/jwt";
 import { PaginationParamas } from "../common/middleware/pagination";
-import z from "zod";
 
 declare module "express-serve-static-core" {
   export interface Request {
